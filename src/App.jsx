d762507335fe774c78bd923b688649e2ababcd0b
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2026-02-23 16:09:23 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Discover from "./components/Discover";
 import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
